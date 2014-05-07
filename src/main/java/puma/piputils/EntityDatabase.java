0a59e9ac06da2be3cd24ca8d5452aca53fe2fa25
Jasper Bogaerts <jasper.bogaerts@cs.kuleuven.be>
2014-05-07 16:31:38 +0200

    Inner join and indexing on queries

--- src/main/java/puma/piputils/EntityDatabase.java
+++ src/main/java/puma/piputils/EntityDatabase.java
@@ -18,11 +18,11 @@ import java.util.logging.Logger;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class EntityDatabase {
-	
+
 	private static EntityDatabase instance;
-	
+
 	public static EntityDatabase getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new EntityDatabase();
 		}
 		return instance;
@@ -34,27 +34,20 @@ public class EntityDatabase {
 
 	private static final Logger logger = Logger.getLogger(EntityDatabase.class
 			.getName());
-	private static final String DEFAULT_CONNECTION = "jdbc:mysql://localhost:3306/puma-mgmt";
-	private static final String DB_USER = "root";
-	private static final String DB_PASSWORD = "root";
-	private static String DB_CONNECTION;
+	private static final String DB_USER = "admin";
+	private static final String DB_PASSWORD = "admin";
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/puma-mgmt";
 
-	
 	private static ComboPooledDataSource cpds;
+
 	/**
 	 * Initializes this new EntityDatabase. Does not open a connection yet.
 	 */
 	private EntityDatabase() {
-		// Set the connection
-		if (System.getProperty("puma.attr.db.connection") == null)
-			DB_CONNECTION = DEFAULT_CONNECTION;
-		else
-			DB_CONNECTION = System.getProperty("puma.attr.db.connection");
-		logger.info("Connection set at [" + DB_CONNECTION + "]. Initializing pool of connections...");
 		try {
 			cpds = new ComboPooledDataSource();
-			cpds.setDriverClass( "com.mysql.jdbc.Driver" );
-			cpds.setJdbcUrl( DB_CONNECTION );			
+			cpds.setDriverClass("com.mysql.jdbc.Driver");
+			cpds.setJdbcUrl(DB_CONNECTION);
 			cpds.setMaxPoolSize(30);
 			cpds.setMinPoolSize(1);
 			cpds.setUser(DB_USER);
@@ -69,20 +62,25 @@ public class EntityDatabase {
 	/**************************
 	 * DATABASE OPERATIONS
 	 */
-	
+
 	private Connection conn = null;
-	
+
+	private PreparedStatement getStringAttributeStmt = null;
+	private PreparedStatement getDataTypeStmt = null;
+	private PreparedStatement getFamilyIdStmt = null;
+	private PreparedStatement getSupportedXACMLAttributeIdsStmt = null;
+
 	/**
-	 * Sets up the connection to the database in read/write mode. 
-	 * Autocommit is disabled for this connection, so know you have to commit yourself!
+	 * Sets up the connection to the database in read/write mode. Autocommit is
+	 * disabled for this connection, so know you have to commit yourself!
 	 */
 	public void open() {
 		open(false);
 	}
-	
+
 	/**
-	 * Sets up the connection to the database in given mode. 
-	 * Autocommit is disabled for this connection, so know you have to commit yourself!
+	 * Sets up the connection to the database in given mode. Autocommit is
+	 * disabled for this connection, so know you have to commit yourself!
 	 */
 	public void open(boolean readOnly) {
 		try {
@@ -90,11 +88,15 @@ public class EntityDatabase {
 			conn.setReadOnly(readOnly);
 			conn.setAutoCommit(false);
 			conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+			getStringAttributeStmt = this.conn.prepareStatement("SELECT SP_ATTR.value FROM SP_ATTRTYPE USE INDEX (familyById) INNER JOIN SP_ATTR ON SP_ATTR.family_id=SP_ATTRTYPE.id AND SP_ATTRTYPE.xacmlIdentifier=? and SP_ATTR.user_id=?");
+			getDataTypeStmt = this.conn.prepareStatement("SELECT dataType FROM SP_ATTRTYPE USE INDEX (familyById) WHERE xacmlIdentifier=?");			
+			getFamilyIdStmt = this.conn.prepareStatement("SELECT id FROM SP_ATTRTYPE USE INDEX (familyById) WHERE xacmlIdentifier=?");
+			getSupportedXACMLAttributeIdsStmt = this.conn.prepareStatement("SELECT xacmlIdentifier FROM SP_ATTRTYPE");
 		} catch (SQLException e) {
 			logger.log(Level.SEVERE, "Cannot open connection.", e);
 		}
 	}
-	
+
 	/**
 	 * Commits all operations.
 	 */
@@ -105,106 +107,122 @@ public class EntityDatabase {
 			logger.log(Level.SEVERE, "Cannot commit.", e);
 		}
 	}
-	
+
 	/**
 	 * Closes the connection to the database.
 	 */
 	public void close() {
 		try {
+			if (getStringAttributeStmt != null) {
+				getStringAttributeStmt.close();
+			}
+			if (getDataTypeStmt != null) {
+				getDataTypeStmt.close();
+			}
+			if (getFamilyIdStmt != null) {
+				getFamilyIdStmt.close();
+			}
+			if (getSupportedXACMLAttributeIdsStmt != null) {
+				getSupportedXACMLAttributeIdsStmt.close();
+			}
 			conn.close();
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Error when closing connection to the database.", e);
+			logger.log(Level.SEVERE,
+					"Error when closing connection to the database.", e);
 		}
 	}
-	
+
 	/**
 	 * Fetches all supported XACML attribute ids from the database.
 	 */
 	public Set<String> getSupportedXACMLAttributeIds() {
 		Set<String> result = new HashSet<String>();
+		ResultSet queryResult = null;
 		try {
-			String query = "SELECT xacmlIdentifier FROM SP_ATTRTYPE";
-			PreparedStatement stmt;
-				stmt = this.conn.prepareStatement(query);
-			
-			ResultSet queryResult = stmt.executeQuery();
-			while (queryResult.next())
+			queryResult = getSupportedXACMLAttributeIdsStmt.executeQuery();
+			while (queryResult.next()) {
 				result.add(queryResult.getString("xacmlIdentifier"));
+			}
 		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Could not fetch xacml attribute identifiers", e);
+			logger.log(Level.SEVERE,
+					"Could not fetch xacml attribute identifiers", e);
+		} finally {
+			if(queryResult != null) {
+				try {
+					queryResult.close();
+				} catch (SQLException e) {
+					// nothing to do
+					e.printStackTrace();
+				}
+			}
 		}
 		return result;
 	}
-	
-	/**
-	 * Fetches the data type of the attribute family with given id from the database.
-	 * @return the datatype for the specified id. If multiple entities were found for the given id, then a random entity is chosen and has its datatype returned. If no id was found in the database, then String is returned.
-	 */
-	public DataType getDataType(String attributeId) {
-		// QUESTION Jasper @ Maarten: Met attributeId wordt xacml identifier bedoeld neem ik aan? 
-		List<String> result = new ArrayList<String>();
-		try {
-			String query = "SELECT dataType FROM SP_ATTRTYPE WHERE xacmlIdentifier=?";
-			PreparedStatement stmt;
-				stmt = this.conn.prepareStatement(query);
-			stmt.setString(1, attributeId);
-			ResultSet queryResult = stmt.executeQuery();
-			while (queryResult.next())
-				result.add(queryResult.getString("dataType"));
-			if (!result.isEmpty()) {
-				if (result.size() > 1)
-					logger.warning("Multiple xacml identifiers: " + attributeId + ". Fetching random family.");
-				String type = result.get(0);
-				return DataType.valueOf(type);
-			}
-		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Could not fetch xacml attribute identifiers", e);
-		} catch (IllegalArgumentException e) {
-			logger.log(Level.SEVERE, "Illegal datatype found in database", e);
-		}
-		return DataType.String;	// QUESTION Jasper @ Maarten: wat moet ik eigenlijk in dit geval het beste doen? Hiervoor ken ik de SunXACML internals nog niet goed genoeg...
-	}
-	
-	private Long getFamilyId(String xacmlIdentifier) {
-		List<Long> result = new ArrayList<Long>();
-		try {
-			String query = "SELECT id FROM SP_ATTRTYPE WHERE xacmlIdentifier=?";
-			PreparedStatement stmt;
-				stmt = this.conn.prepareStatement(query);
-			stmt.setString(1, xacmlIdentifier);
-			ResultSet queryResult = stmt.executeQuery();
-			while (queryResult.next())
-				result.add(queryResult.getLong("id"));
-			if (!result.isEmpty()) {
-				if (result.size() > 1)
-					logger.warning("Multiple xacml identifiers: " + xacmlIdentifier + ". Fetching random family and hoping for the best.");
-				Long id = result.get(0);
-				return id;
-			}
-		} catch (SQLException e) {
-			logger.log(Level.SEVERE, "Could not fetch xacml attribute identifiers", e);
-		} catch (NumberFormatException e) {
-			logger.log(Level.SEVERE, "Illegal family id found in database", e);
-		}
-		return null;
-	}
 
 	/**
-	 * Fetches a string attribute from the database using the connection of this database.
-	 * Does NOT commit or close.
+	 * Fetches the data type of the attribute family with given id from the
+	 * database.
+	 * 
+	 * @return the datatype for the specified id. If multiple entities were
+	 *         found for the given id, then a random entity is chosen and has
+	 *         its datatype returned. If no id was found in the database, then
+	 *         String is returned.
+	 */
+	public DataType getDataType(String attributeId) {
+		// QUESTION Jasper @ Maarten: Met attributeId wordt xacml identifier
+		// bedoeld neem ik aan?
+		List<String> result = new ArrayList<String>();
+		ResultSet queryResult = null;
+		try {
+			getDataTypeStmt.setString(1, attributeId);
+			queryResult = getDataTypeStmt.executeQuery();
+			while (queryResult.next()) {
+				result.add(queryResult.getString("dataType"));
+			}
+			if (!result.isEmpty()) {
+				if (result.size() > 1)
+					logger.warning("Multiple xacml identifiers: " + attributeId
+							+ ". Fetching random family.");
+				String type = result.get(0);
+				return DataType.valueOf(type);
+			}			
+		} catch (SQLException e) {
+			logger.log(Level.SEVERE,
+					"Could not fetch xacml attribute identifiers", e);
+		} catch (IllegalArgumentException e) {
+			logger.log(Level.SEVERE, "Illegal datatype found in database", e);
+		} finally {
+			if(queryResult != null) {
+				try {
+					queryResult.close();
+				} catch (SQLException e) {
+					// nothing to do
+					e.printStackTrace();
+				}
+			}
+		}
+		return DataType.String; // QUESTION Jasper @ Maarten: wat moet ik
+								// eigenlijk in dit geval het beste doen?
+								// Hiervoor ken ik de SunXACML internals nog
+								// niet goed genoeg...
+	}
+
+
+	/**
+	 * Fetches a string attribute from the database using the connection of this
+	 * database. Does NOT commit or close.
 	 */
 	public Set<String> getStringAttribute(String entityId, String key) {
-		// QUESTION: Jasper @ Maarten: ik neem wederom aan dat het hier gaat om de xacmlIdentifier als er een key wordt doorgegeven?
+		// QUESTION: Jasper @ Maarten: ik neem wederom aan dat het hier gaat om
+		// de xacmlIdentifier als er een key wordt doorgegeven?
+		ResultSet queryResult = null;
 		try {
-			Long familyId = getFamilyId(key);
-			logger.info("Fetching attribute with family id [" + familyId + "] and user id [" + entityId + "]...");
-			String query = "SELECT value FROM SP_ATTR WHERE family_id=? AND user_id=?";
-			PreparedStatement stmt;
-				stmt = this.conn.prepareStatement(query);
-			stmt.setLong(1, familyId);
-			stmt.setLong(2, Long.valueOf(entityId));
-			ResultSet queryResult = stmt.executeQuery();
-			
+			logger.info("Fetching attribute with family [" + key
+					+ "] and user id [" + entityId + "]...");
+			getStringAttributeStmt.setString(1, key);
+			getStringAttributeStmt.setLong(2, Long.valueOf(entityId));
+			queryResult = getStringAttributeStmt.executeQuery();
+
 			// process the result
 			Set<String> r = new HashSet<String>();
 			String next;
@@ -216,31 +234,40 @@ public class EntityDatabase {
 		} catch (SQLException e) {
 			logger.log(Level.SEVERE, "Cannot execute query.", e);
 			throw new RuntimeException(e);
+		} finally {
+			if(queryResult != null) {
+				try {
+					queryResult.close();
+				} catch (SQLException e) {
+					// nothing to do
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
 	/**
-	 * Fetches an integer attribute from the database using the connection of this database.
-	 * Does NOT commit or close.
+	 * Fetches an integer attribute from the database using the connection of
+	 * this database. Does NOT commit or close.
 	 */
 	public Set<Integer> getIntegerAttribute(String entityId, String key) {
 		Set<String> strings = getStringAttribute(entityId, key);
 		Set<Integer> result = new HashSet<Integer>();
-		for(String s: strings) {
+		for (String s : strings) {
 			result.add(Integer.valueOf(s));
 		}
 		return result;
 	}
 
 	/**
-	 * Fetches a boolean attribute from the database using the connection of this database.
-	 * Does NOT commit or close.
+	 * Fetches a boolean attribute from the database using the connection of
+	 * this database. Does NOT commit or close.
 	 */
 	public Set<Boolean> getBooleanAttribute(String entityId, String key) {
 		Set<String> strings = getStringAttribute(entityId, key);
 		Set<Boolean> result = new HashSet<Boolean>();
-		for(String s: strings) {
-			if(s.equals("true")) {
+		for (String s : strings) {
+			if (s.equals("true")) {
 				result.add(true);
 			} else {
 				result.add(false);
@@ -250,14 +277,14 @@ public class EntityDatabase {
 	}
 
 	/**
-	 * Fetches a boolean attribute from the database using the connection of this database.
-	 * Does NOT commit or close.
+	 * Fetches a boolean attribute from the database using the connection of
+	 * this database. Does NOT commit or close.
 	 */
 	public Set<Date> getDateAttribute(String entityId, String key) {
 		Set<String> strings = getStringAttribute(entityId, key);
 		Set<Date> result = new HashSet<Date>();
 		DateFormat df = DateFormat.getInstance();
-		for(String s: strings) {
+		for (String s : strings) {
 			try {
 				Date d = df.parse(s);
 				result.add(d);
